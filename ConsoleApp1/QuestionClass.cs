78c2b3df7fe5073735e5763420b6f8b94945200c
veenaksb <89020821+veenaksb@users.noreply.github.com>
2021-08-19 00:58:26 +0530

    step7

---
+++ ConsoleApp1/QuestionClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class QuestionClass
+    {
+        public static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+    }
+}
