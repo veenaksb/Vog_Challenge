1b6f174b04e0b91564126bdcecd17fe86631599a
veenaksb <89020821+veenaksb@users.noreply.github.com>
2021-08-19 00:18:21 +0530

    step3

---
+++ VogCodeChallenge.API/Controller/EmployeesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VogCodeChallenge.API.Models;
+
+namespace VogCodeChallenge.API.Controller
+{
+    [ApiController]
+    public class EmployeesController
+    {
+        IList<Employees> emplst;
+        public EmployeesController()
+        {
+            emplst = Enumerable.Range(1, 5).Select(index => new Employees
+            {
+                FirstName = "veena" + index,
+                LastName = "kb" + index,
+                JobTitle = "developer" + index,
+                Address = "Bangalore" + index,
+                DepartmentId = index,
+            })
+               .ToArray();
+        }
+
+        [HttpGet]
+        [Route("api/[Controller]/[action]")]
+        [ActionName("GetAll")]
+        public IEnumerable<Employees> GetAll()
+        {
+            return emplst;
+        }
+
+       
+        [HttpGet]
+        [Route("api/[Controller]/[action]")]
+        //[ActionName("ListAll")]
+        public IList<Employees> ListAll()
+        {
+            return emplst;
+        }
+    }
+}
