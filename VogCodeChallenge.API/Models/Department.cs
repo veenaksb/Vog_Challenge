ea78ee030c7ce677a25fde318a9a043e9a298db6
veenaksb <89020821+veenaksb@users.noreply.github.com>
2021-08-19 00:10:17 +0530

    step2

---
+++ VogCodeChallenge.API/Models/Department.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Models
+{
+    public class Department
+    {
+        private string name;
+        private string address;
+        private List<Employees> empLst;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public List<Employees> LstEmployee
+        {
+            get { return empLst; }
+            set { empLst = value; }
+        }
+    }
+}
---
+++ VogCodeChallenge.API/Models/Employees.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Models
+{
+    public class Employees
+    {
+        private string firstname;
+        private string lastname;
+        private string jobtitle;
+        private string address;
+        private int departmentId;
+
+        public string FirstName
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+
+        public string JobTitle
+        {
+            get { return jobtitle; }
+            set { jobtitle = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public int DepartmentId
+        {
+            get { return departmentId; }
+            set { departmentId = value; }
+        }
+    }
+}
