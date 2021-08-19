a714c1733f8276322743ff05529cd6b77b41a103
veenaksb <89020821+veenaksb@users.noreply.github.com>
2021-08-19 13:54:37 +0530

    step8

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,11 +11,70 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             var enumerator = QuestionClass.NamesList.GetEnumerator();
-            while(enumerator.MoveNext())
+            while (enumerator.MoveNext())
             {
                 Console.WriteLine(enumerator.Current);
             }
+
+            var answer = TESTModule("1");
         }
 
-           }
+        private static string TESTModule(string val)
+        {
+            //var decisepa = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            int intval = 0;
+            bool isString = false;
+            if (int.TryParse(val, out intval))
+            {
+                isString = false;
+            }
+            else
+            {
+                isString = true;
+            }
+
+            switch (val)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                    return (int.Parse(val) * 2).ToString();
+
+                case "1.0f":
+                case "2.0f":
+                    return "3.0f";
+
+                default:
+                    if (isString)
+                    {
+                        bool isDigitPresent = val.Any(c => char.IsDigit(c));
+                        if (isDigitPresent)
+                        {
+                            return val;
+                        }
+                        else
+                        {
+                            return val.ToUpper();
+                        }
+                    }
+                    else
+                    {
+                        if (intval > 4)
+                        {
+                            return (intval * 3).ToString();
+                        }
+                        if (intval < 1)
+                        {
+                            throw new Exception("Invalid input.");
+                        }
+                        else
+                        {
+                            return val;
+                        }
+                    }
+            }
+        }
+
+    }
 }
